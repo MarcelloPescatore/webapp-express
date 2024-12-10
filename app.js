2b48e27ee92325f086abcf738301dacd5bf00fa5
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-12-10 15:25:38 +0100

    link the app to the db and check the connection

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const server = express()
+const connection = require('./database/connection')
 
 
 const HOST = process.env.HOST || 'http://localhost'
 const PORT = process.env.PORT || '3001'
+
+
+server.listen(PORT, () => {
+    console.log(`Server in ascolto ${HOST}:${PORT}`);
+})
